3347be8b3397fb377fbce414fb108c4ec03cc36f
Isaiah M. Bernal <isaiahmbernal@gmail.com>
2022-09-20 11:44:14 -0500

    Framer Motion

--- frontend/src/pages/MissingPage.jsx
+++ frontend/src/pages/MissingPage.jsx
@@ -7,6 +7,9 @@ const MissingPage = () => {
         <h1>404</h1>
         <h1>Page Does Not Exist</h1>
         <div className="bg-center bg-cover bg-skeleton-guy w-full h-[60vh] rounded-lg shadow-2xl"></div>
+        <audio controls>
+          <source src="/bruh.mp3" type="audio/mp3"/>
+        </audio>
       </div>
     </PageLayout>
   );
