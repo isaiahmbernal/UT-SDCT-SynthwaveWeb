dcc2018ee7130d248ead1d34b6ea6084b74f1fe3
ellekathryn <ellemisencik@gmail.com>
2022-09-22 17:30:33 -0500

    Elle's First Commit

--- frontend/src/pages/CountdownPage.jsx
+++ frontend/src/pages/CountdownPage.jsx
@@ -42,7 +42,7 @@ const CountdownPage = () => {
       <div className="relative bg-pink-grid bg-center flex flex-col items-center w-full h-full p-4">
         {/* Component Code Goes Here */}
         <p className="text-white">Countdown Page</p>
-        <p className="text-white">Test test, is this thing on</p>
+        <p className="text-white">Test, is this thing on</p>
         {/* <p className="text-white">{code}</p> */}
       </div>
     </PageLayout>
