37669deb0042325c488b559fc1f363a60cdd8aa4
lingming kid <yanlianming@utexas.edu>
2022-10-27 00:54:57 -0500

    Update BadgeInfo.jsx

--- frontend/src/pages/Map/BadgeInfo.jsx
+++ frontend/src/pages/Map/BadgeInfo.jsx
@@ -1,33 +1,36 @@
 import { motion } from "framer-motion";
 
 const badges = [
-  {src: "/images/icons/entrance_star.png", alt: "/images/icons/entrance.png",},
-  {src: "/images/icons/lambo_star.png", alt: "/images/icons/lambo.png",},
-  {src: "/images/icons/live_performance_star.png", alt: "/images/icons/live_performance.png",},
-  {src: "/images/icons/outdoor_projection_star.png", alt: "/images/icons/outdoor_projection.png",},
-  {src: "/images/icons/stage_star.png", alt: "/images/icons/stage.png",},
+  { src: "/images/icons/entrance_star.png", alt: "/images/icons/entrance.png", },
+  { src: "/images/icons/lambo_star.png", alt: "/images/icons/lambo.png", },
+  { src: "/images/icons/live_performance_star.png", alt: "/images/icons/live_performance.png", },
+  { src: "/images/icons/outdoor_projection_star.png", alt: "/images/icons/outdoor_projection.png", },
+  { src: "/images/icons/stage_star.png", alt: "/images/icons/stage.png", },
 ];
 
 const BadgeInfo = ({ setBadgeShow, scannedQR, totalQR }) => {
+  scannedQR = 0;
   return (
     <motion.div
       initial={{ x: 1000 }}
       animate={{ x: 0 }}
       exit={{ x: 1000 }}
       transition={{ duration: 0.15 }}
-      className="z-50 fixed flex flex-col bg-black/90 min-w-[100vw] min-h-[100vh] p-8"
+      className="z-50 fixed flex flex-col bg-black/90 min-w-[100vw] min-h-[100vh]"
     >
-      <div className="grid grid-rows-2 grid-cols-2 gap-8 self-center">
+      <div className="m-8 grid grid-rows-3 grid-cols-2 gap-8 self-center">
         {badges.map((image, index) => (
-          <img src={index < scannedQR ? image.src : image.alt} className="bg-purple-900/30 rounded-xl shadow-md w-[10rem] h-[10rem]" key={index}/>
+          <img src={index < scannedQR ? image.src : image.alt} className="bg-purple-900/30 rounded-3xl" key={index} />
         ))}
       </div>
-      <button
-        className="bg-purple-800 text-white absolute bottom-[2%] left-[25%] w-[50%] h-[3rem] rounded-lg shadow-md"
-        onClick={() => setBadgeShow(false)}
-      >
-        Back
-      </button>
+      <div className="absolute bottom-[5%] m-8 self-center content-center">
+        <button
+          className="w-full py-4 px-8 bg-black/30 text-purple-300 text-3xl font-bold border-4 border-purple-800 rounded-xl"
+          onClick={() => setBadgeShow(false)}
+        >
+          Back
+        </button>
+      </div>
     </motion.div>
   );
 };
