2f08a74c6c5e47d35ed30136e8612761863e94e2
isaiahmbernal <isaiahmbernal@gmail.com>
2022-11-01 14:13:55 -0500

    Info Box Sizing Changes

--- frontend/src/pages/Map/GoodJob.jsx
+++ frontend/src/pages/Map/GoodJob.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 const GoodJob = ({ scannedQR, totalQR }) => {
   return (
     <motion.section
-      className="fixed top-[1%] w-[97%] z-[100] h-[20vh] shadow-md bg-white text-black flex flex-col items-center justify-center text-xl font-bold text-center p-4 gap-4 rounded-xl"
+      className="fixed top-[1%] max-w-[30rem] w-[97%] z-[100] h-[20vh] shadow-md bg-white text-black flex flex-col items-center justify-center text-xl font-bold text-center p-4 gap-4 rounded-xl"
       initial={{ y: -500 }}
       animate={{ y: 0 }}
       exit={{ y: -500 }}
