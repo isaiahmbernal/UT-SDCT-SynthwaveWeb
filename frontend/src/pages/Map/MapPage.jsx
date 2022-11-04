97a5ba11abe8af27e038a105bdabefcd48c68834
isaiahmbernal <isaiahmbernal@gmail.com>
2022-11-04 18:07:24 -0500

    New Codes

--- frontend/src/pages/Map/MapPage.jsx
+++ frontend/src/pages/Map/MapPage.jsx
@@ -12,54 +12,81 @@ const MapPage = () => {
   const markers = {
     lambo: {
       name: 'Lambo',
-      info: 'This is a lamborghini, wow, so cool!',
+      info: "Woah, where'd they get a lamborghini! 🤩",
       className:
-        'absolute fixed left-[45%] top-[48%] w-[5rem] h-[5rem] cursor-pointer',
+        'absolute fixed left-[49%] top-[49%] w-[3.5rem] h-[3.5rem] cursor-pointer',
       transition: { delay: 1.25 },
       src: '/images/icons/lambo.png',
       alt: '/images/icons/lambo_star.png',
     },
     stage: {
-      name: 'Stage',
-      info: 'Cumque quas libero tempora magni beatae saepe nam. Voluptate et pariatur vel aperiam ipsum quisquam. Provident corporis eos recusandae quisquam veritatis corrupti non. Cumque quas libero tempora magni beatae saepe nam. Voluptate et pariatur vel aperiam ipsum quisquam. Provident corporis eos recusandae quisquam veritatis corrupti non.',
+      name: 'Interactive Music',
+      info: 'Time to interact with some music 😎',
       className:
-        'absolute left-[5%] top-[42%] w-[5rem] h-[5rem] cursor-pointer',
+        'absolute left-[5%] top-[42%] w-[3.5rem] h-[3.5rem] cursor-pointer',
       transition: { delay: 0.75 },
       src: '/images/icons/stage.png',
       alt: '/images/icons/stage_star.png',
     },
-    entrance: {
-      name: 'Entrance',
-      info: "Entrance to the show, maybe there's some cool stuff here",
+    lobby: {
+      name: 'Lobby',
+      info: "Maybe there's some cool stuff here 👀",
       className:
-        'absolute left-[24%] top-[51%] w-[5rem] h-[5rem] cursor-pointer',
+        'absolute left-[55%] top-[43%] w-[3.5rem] h-[3.5rem] cursor-pointer',
       transition: { delay: 1 },
       src: '/images/icons/entrance.png',
       alt: '/images/icons/entrance_star.png',
     },
-    concert: {
+    finale: {
       name: 'Live Performance',
-      info: "You're probably not gonna wanna miss this preshow",
+      info: "It's time to ride the synthwave 🏄",
       className:
-        'absolute left-[18%] top-[1%] w-[5rem] h-[5rem] cursor-pointer',
+        'absolute left-[18%] top-[1%] w-[3.5rem] h-[3.5rem] cursor-pointer',
       transition: { delay: 0.25 },
       src: '/images/icons/live_performance.png',
       alt: '/images/icons/live_performance_star.png',
     },
     projection: {
       name: 'Outdoor Projection',
-      info: 'Wow, look how pretty!',
+      info: 'Wow, how pretty 📽',
       className:
-        'absolute left-[32%] top-[37%] w-[5rem] h-[5rem] cursor-pointer',
+        'absolute left-[38%] top-[39%] w-[3.5rem] h-[3.5rem] cursor-pointer',
       transition: { delay: 0.5 },
       src: '/images/icons/outdoor_projection.png',
       alt: '/images/icons/outdoor_projection_star.png',
     },
     games: {
-      name: 'Block World',
-      info: "Block world, it can't get any better than this",
+      name: 'Games',
+      info: "1v1 me, let's go",
       className:
-        'absolute left-[60%] top-[70%] w-[5rem] h-[5rem] cursor-pointer',
+        'absolute left-[29%] top-[51%] w-[3.5rem] h-[3.5rem] cursor-pointer',
+      transition: { delay: 1.5 },
+      src: '/images/icons/games.png',
+      alt: '/images/icons/games_star.png',
+    },
+    river: {
+      name: 'River of Light',
+      info: "Please, don't swim",
+      className:
+        'absolute left-[60%] top-[72%] w-[3.5rem] h-[3.5rem] cursor-pointer',
+      transition: { delay: 1.5 },
+      src: '/images/icons/games.png',
+      alt: '/images/icons/games_star.png',
+    },
+    bike: {
+      name: 'Light Bike',
+      info: "Please, don't go for a ride",
+      className:
+        'absolute left-[66%] top-[58%] w-[3.5rem] h-[3.5rem] cursor-pointer',
+      transition: { delay: 1.5 },
+      src: '/images/icons/games.png',
+      alt: '/images/icons/games_star.png',
+    },
+    steprepeat: {
+      name: 'Step & Repeat',
+      info: "Smile and wave, smile and wave.",
+      className:
+        'absolute left-[51%] top-[55%] w-[3.5rem] h-[3.5rem] cursor-pointer',
       transition: { delay: 1.5 },
       src: '/images/icons/games.png',
       alt: '/images/icons/games_star.png',
@@ -113,11 +140,17 @@ const MapPage = () => {
     } else if (code) {
       if (code == 'finale') {
         localStorage.setItem('finale', true);
+        console.log('*** That QR code looks legit!');
+        console.log('Progress ', code + ':', progress[code]);
+        progress[code] = true;
+        console.log('New Progress ', code + ':', progress[code]);
+        localStorage.setItem('codes', JSON.stringify(progress));
+        console.log('New Local Storage:', progress);
+        localStorage.setItem('recentScan', code);
         setFinale(true);
       } else if (Object.keys(progress).includes(code)) {
         console.log('*** That QR code looks legit!');
         console.log('Progress ', code + ':', progress[code]);
-
         progress[code] = true;
         console.log('New Progress ', code + ':', progress[code]);
         localStorage.setItem('codes', JSON.stringify(progress));
