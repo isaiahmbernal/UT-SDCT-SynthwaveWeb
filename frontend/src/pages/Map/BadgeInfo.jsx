0d2b63d7041d7de9385e367bd79a6a44f40f07d3
lingming kid <yanlianming@utexas.edu>
2022-11-10 01:05:07 -0600

    sync with main branch

--- frontend/src/pages/Map/BadgeInfo.jsx
+++ frontend/src/pages/Map/BadgeInfo.jsx
@@ -1,35 +1,78 @@
 import { motion } from "framer-motion";
 
-const badges = [
-  { src: "/images/icons/entrance_star.png", alt: "/images/icons/entrance.png", },
-  { src: "/images/icons/lambo_star.png", alt: "/images/icons/lambo.png", },
-  { src: "/images/icons/live_performance_star.png", alt: "/images/icons/live_performance.png", },
-  { src: "/images/icons/outdoor_projection_star.png", alt: "/images/icons/outdoor_projection.png", },
-  { src: "/images/icons/stage_star.png", alt: "/images/icons/stage.png", },
-];
-
-const BadgeInfo = ({ setBadgeShow, scannedQR, totalQR }) => {
-  scannedQR = 0;
+const BadgeInfo = ({ setBadgeShow, scannedQR, totalQR, markers, progress }) => {
   return (
     <motion.div
       initial={{ x: 1000 }}
       animate={{ x: 0 }}
       exit={{ x: 1000 }}
-      transition={{ duration: 0.15 }}
-      className="z-50 fixed flex flex-col bg-black/90 min-w-[100vw] min-h-[100vh]"
+      transition={{ duration: .1 }}
+      className="z-[200] fixed flex flex-col justify-between items-center bg-black w-screen h-screen px-8 py-4"
     >
-      <div className="m-8 grid grid-rows-3 grid-cols-2 gap-8 self-center">
-        {badges.map((image, index) => (
-          <img src={index < scannedQR ? image.src : image.alt} className="bg-purple-900/30 rounded-3xl" key={index} />
-        ))}
-      </div>
-      <div className="absolute bottom-[5%] m-8 self-center content-center">
-        <button
-          className="w-full py-4 px-8 bg-black/30 text-purple-300 text-3xl font-bold border-4 border-purple-800 rounded-xl"
-          onClick={() => setBadgeShow(false)}
-        >
-          Back
-        </button>
+      <div className="flex flex-col items-center w-full h-full gap-6">
+        <div className="max-w-[23rem] w-full min-h-[3rem] flex gap-4 justify-center items-center">
+          <motion.div
+            className="relative w-[4rem] h-full"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <div className="z-[-1] absolute w-full h-full rounded-xl blur-[.5rem] animate-pulse bg-neonBlue"></div>
+            <motion.button
+              className="bg-black text-white text-xl w-full h-full rounded-xl shadow-md flex justify-center items-center border-double border-4 border-neonBlue"
+              onClick={() => setBadgeShow(false)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={"w-6 h-6 focus:invisible"}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z"
+                />
+              </svg>
+            </motion.button>
+          </motion.div>
+          <div className="relative w-full h-full">
+            <div className="z-[-1] rounded-xl absolute w-full h-full flex justify-center items-center blur-[.5rem] animate-pulse bg-neonBlue"></div>
+            <div className="bg-black rounded-xl w-full h-full flex justify-center items-center border-double border-4 border-neonBlue">
+              <h1 className="text-white text-xl">Badge Progress</h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-6 min-h-[0rem] max-w-[17rem] w-full overflow-y-scroll p-2">
+          {Object.entries(markers).map(([key, value]) => (
+            <motion.div
+              className="relative"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              {progress[key] ? (
+                <div className="-z-10 w-full h-full absolute blur-[.5rem] bg-gradient-to-r from-pink-500 to-blue-500 rounded-3xl"></div>
+              ) : (
+                <div className="-z-10 w-full h-full absolute blur-[.5rem] bg-gray-400 rounded-3xl"></div>
+              )}
+              <div
+                className={`${
+                  progress[key] ? 'bg-black' : 'bg-black/70'
+                } z-[1] w-full h-full p-1 rounded-3xl shadow-md`}
+                // key={value}
+              >
+                <img
+                  src={progress[key] ? value.alt : value.src}
+                  className={`${
+                    progress[key] ? 'animate-wiggle' : 'brightness-[6%]'
+                  }`}
+                />
+              </div>
+            </motion.div>
+          ))}
+        </div>
       </div>
     </motion.div>
   );
